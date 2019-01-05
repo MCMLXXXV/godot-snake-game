90cfa280dae4f3bfa41ac600d9f3063cb0ed7a4e
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2019-01-05 18:06:37 -0200

    Reorganized, renamed nodes.

--- ui/game/game.gd
+++ ui/game/game.gd
@@ -4,16 +4,16 @@ Game
 
 Manages the Game screen UI.
 """
-extends Node
+extends MarginContainer
 
 
 """
 Quick retrieval of a few UI elements.
 """
-onready var grid = $MarginContainer/Layout/GridContainer/Grid
-onready var animation = $AnimationPlayer
-onready var score_label = $MarginContainer/Layout/Header/Points
-onready var game_over_label = $MarginContainer/Layout/Header/GameOverLabel
+onready var grid = $main_layout/grid_container/grid
+onready var animation = $animation_player
+onready var score_label = $main_layout/header/points
+onready var game_over_label = $main_layout/header/game_over_label
 
 
 """
@@ -27,8 +27,7 @@ func update_score(points):
 Callback of the game over signal.
 """
 func game_over():
-	game_over_label.show()
-	animation.play("blink-score")
+	animation.play("blink-game-over")
 	Global.high_score = grid.score
 	yield(get_tree().create_timer(3.0), "timeout")
 	go_to_title_screen()
