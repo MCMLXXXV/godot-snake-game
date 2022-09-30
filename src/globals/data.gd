b049bf441763296f9950b0dee1973abed91ab25c
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2022-09-29 22:38:55 -0300

    Update to API changes of Godot 4.0-beta2.

--- src/globals/data.gd
+++ src/globals/data.gd
@@ -3,21 +3,18 @@ extends Node
 
 
 ## Loads the data from a given JSON file.
-static func load_json(json_path: String, default = null):
-	var result = default
-	if File.file_exists(json_path):
-		var file := File.new()
-		if file.open(json_path, File.READ) == OK:
+static func load_json(filename: String, default: Variant = null) -> Variant:
+	if FileAccess.file_exists(filename):
+		var file := FileAccess.open(filename, FileAccess.READ)
+		if FileAccess.get_open_error() == OK:
 			var json := JSON.new()
 			if json.parse(file.get_as_text()) == OK:
-				result = json.get_data()
-			file.close()
-	return result
+				return json.get_data()
+	return default
 
 
 ## Saves the data to the given JSON file.
-static func save_json(json_path: String, data) -> void:
-	var file := File.new()
-	if file.open(json_path, File.WRITE) == OK:
+static func save_json(filename: String, data: Variant) -> void:
+	var file := FileAccess.open(filename, FileAccess.WRITE)
+	if FileAccess.get_open_error() == OK:
 		file.store_string(JSON.stringify(data))
-		file.close()
