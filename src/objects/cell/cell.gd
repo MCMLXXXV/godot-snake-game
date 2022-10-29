d70447f75c137d1c65fff243f70ae2417e5e2dab
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2022-10-29 11:56:04 -0300

    Code revisions.
    - Removed an unnecessary negative bounds check on Cell's `cell`
      property.

--- src/objects/cell/cell.gd
+++ src/objects/cell/cell.gd
@@ -4,12 +4,12 @@ extends Sprite
 ## This grid cell size.
 const CELL_SIZE: int = 16
 
-## The grid cell this piece of food occupies on the grid.
+## The coordinates this cell occupies on the grid.
 var cell: Vector2 = Vector2.ZERO setget set_cell
 
 
 ## Sets this cell position on the grid.
 func set_cell(value: Vector2) -> void:
-	cell = value if value >= Vector2.ZERO else Vector2.ZERO
+	cell = value
 	# Make sure the sprite is always updated accordingly.
 	position = CELL_SIZE * cell
